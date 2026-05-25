f3b45e243f49433d27d90237c46defea26347a8d
Hilzyyy <164994772+xHilyZ@users.noreply.github.com>
2026-05-26 00:53:06 +1000

    update

---
+++ scripts/gallery.js
@@ -0,0 +1,13 @@
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+
+document.querySelectorAll(".gallery-img").forEach(img => {
+  img.addEventListener("click", () => {
+    lightbox.style.display = "flex";
+    lightboxImg.src = img.src;
+  });
+});
+
+lightbox.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
